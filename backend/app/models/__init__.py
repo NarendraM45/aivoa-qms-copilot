d79f442db809703d54d0fbaba55930950c571319
NarendraM45 <narendra.pro1212@gmail.com>
2026-07-30 00:01:59 +0530

    chore(models): init models package and Base class

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,7 @@
+from app.models.complaint import Complaint
+from app.models.attachment import ComplaintAttachment
+from app.models.extraction_run import AIExtractionRun
+from app.models.chat_message import ChatMessage
+from app.models.duplicate_match import DuplicateMatch
+from app.models.capa_recommendation import CAPARecommendation
+from app.models.audit_trail import AuditTrail
